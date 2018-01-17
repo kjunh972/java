e7d3dce1ecea3f3ce73873402791f0e30e8a536f
kimjunhyeong <kjunh972@naver.com>
2018-01-17 14:59:28 +0900

    배열에서 최소값 구하기

---
+++ 배열에서최소값구하기.java
@@ -0,0 +1,21 @@
+package kjunh972.java.배열에서최소값구하기;
+
+public class 배열에서최소값구하기 {
+
+	public static void main(String[] args) {
+		int i , max = 0;
+		int[] Arr = {1,9,7,10,2};
+		max = Arr[0];
+		for (i=0; i<=4; i++)
+		{
+			
+			if (max>Arr[i])
+			{
+				max=Arr[i];
+			}
+		}
+		System.out.printf("%d",max);
+
+	}
+
+}
