31666c7cbfbdbca73c96997bbf5a34c959a15a76
kjunh972 <kjunh972@gmail.com>
2018-06-26 21:39:47 +0900

    입력된 수가 짝수라면 2로 나누고, 홀수라면 3을 곱하고 1을 더한 다음, 결과로 나온 수에 같은 작업을 1이 될 때까지 반복할 경우 모든 수가 1이 된다는 추측입니다.

---
+++ 콜라츠추측.java
@@ -0,0 +1,35 @@
+package kjunh972.java;
+
+import java.util.Scanner;
+
+public class 콜라츠추측 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자를 입력해주세요 : ");
+		int num = sc.nextInt();
+		int i = 0;
+		
+		while (true)
+		{
+			i++;
+			if (i==500)
+			{
+				num = -1;
+				break;
+			} else if (num==1) {
+				break;
+			} else if (num%2==0) {
+				num = num/2;
+				continue;
+			} else {
+				num = num*3+1;
+				continue;
+			}
+		}
+		
+		System.out.println("결과 : "+num);
+	}
+
+}
